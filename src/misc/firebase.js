6f4acf766438caaace0971084ba657b4f85202fd
AkshayTyagi292 <tyagiakshay292@gmail.com>
2023-07-12 04:13:23 +0530

    Initialise Firebase Project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyCJh-W5vURw0v4CDnODciNmqCiJdA5QEcg",
+    authDomain: "chat-web-app-ceea8.firebaseapp.com",
+    projectId: "chat-web-app-ceea8",
+    storageBucket: "chat-web-app-ceea8.appspot.com",
+    messagingSenderId: "1009038209032",
+    appId: "1:1009038209032:web:875cece3c017fc33aca1bd"
+  };
+
+const app = firebase.initializeApp(config);
